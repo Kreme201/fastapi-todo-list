26d907757feae9d77d87620eb2d99ea39a5aa76a
Kreme201 <kreme201@gmail.com>
2022-12-20 18:12:05 +0900

    response class 적용

--- routers/api/v1/todo.py
+++ routers/api/v1/todo.py
@@ -7,28 +7,26 @@ from crud import todo
 from models.todo import Todo
 
 
+class ResponseDto(BaseModel):
+    success: bool
+
+
 router = APIRouter()
 
 
 @router.post("/")
-async def create_todo(content: str = Form("")):
+async def create_todo(content: str = Form("")) -> ResponseDto:
     if not content:
         raise HTTPException(status_code=404, detail="Content is Required")
 
-    return {
-        "success": todo.create(content),
-    }
+    return ResponseDto(success=todo.create(content))
 
 
 @router.put("/{todo_id}")
-async def toggle_status_todo(todo_id: int):
-    return {
-        "success": todo.update(todo_id),
-    }
+async def toggle_status_todo(todo_id: int) -> ResponseDto:
+    return ResponseDto(success=todo.update(todo_id))
 
 
 @router.delete("/{todo_id}")
-async def delete_todo(todo_id: int):
-    return {
-        "success": todo.delete(todo_id),
-    }
+async def delete_todo(todo_id: int) -> ResponseDto:
+    return ResponseDto(success=todo.delete(todo_id))
