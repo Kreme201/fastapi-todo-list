23ee3be5f5642b9f3103c595840ede7c9bd9df6c
Kreme201 <kreme201@gmail.com>
2022-12-21 00:30:31 +0900

    실행 처리 추가

--- main.py
+++ main.py
@@ -10,3 +10,8 @@ app.mount("/assets", StaticFiles(directory="assets"), name="assets")
 
 app.include_router(views.router, include_in_schema=False)
 app.include_router(api.router, prefix="/api/v1")
+
+if __name__ == "__main__":
+    from uvicorn import run
+
+    run("main:app", reload=True)
