55626e812efc769f69476d3fbcc16bec3536d967
Kreme201 <kreme201@gmail.com>
2022-12-22 15:22:02 +0900

    router 로드 변경

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-from routers import views
-from routers.api.v1 import api
+from routers.views import router as view_router
+from routers.api.v1 import router as api_v1_router
 
 
 app = FastAPI()
 app.mount("/assets", StaticFiles(directory="assets"), name="assets")
 
-app.include_router(views.router, include_in_schema=False)
-app.include_router(api.router, prefix="/api/v1")
+app.include_router(view_router, include_in_schema=False)
+app.include_router(api_v1_router, prefix="/api/v1")
 
 
 if __name__ == "__main__":
--- routers/api/v1/api.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-from . import todo, user, auth
-
-router = APIRouter()
-
-router.include_router(todo.router, prefix="/todo", tags=["Todo"])
-router.include_router(user.router, prefix="/user", tags=["User"])
-router.include_router(auth.router, prefix="/auth", tags=["Auth"])
